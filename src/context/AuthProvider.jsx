857602780c5c41b1b53c947df5bc353104b9627d
ersinn26 <154364329+ersinn26@users.noreply.github.com>
2024-05-02 01:36:54 +0300

    register

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,28 @@
+import React, { createContext, useContext, useState } from "react";
+import { auth } from "../auth/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+const AuthContext = createContext();
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
+const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(false);
+  const createUser = async (email, password) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(userCredential);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const values = { currentUser, createUser };
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
