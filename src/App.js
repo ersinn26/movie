97f612f6951ddefae05ef2dde5672272bd5bf7a1
ersinn26 <154364329+ersinn26@users.noreply.github.com>
2024-05-01 15:44:11 +0300

    add css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import React from "react";
+import Register from "./pages/Register";
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
+    <div>
+      <Register />
     </div>
   );
-}
+};
 
 export default App;
