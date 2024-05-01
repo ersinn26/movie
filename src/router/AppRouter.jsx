d210d67984c252afdaba63bc5d91f3b36f728af6
ersinn26 <154364329+ersinn26@users.noreply.github.com>
2024-05-01 16:27:40 +0300

    route

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Main from "../pages/Main";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import MovieDetail from "../pages/MovieDetail";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/detail/:id" element={<MovieDetail />}></Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default AppRouter;
