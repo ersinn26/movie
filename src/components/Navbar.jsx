26aef046838a1772675850201d1c1dca1c2bb979
ersinn26 <154364329+ersinn26@users.noreply.github.com>
2024-05-03 22:34:15 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ function classNames(...classes) {
 // const currentUser = { displayName: "Ersin üNAL" };
 
 export default function Navbar() {
-  const { currentUser } = useAuthContext();
+  const { currentUser, logOut } = useAuthContext();
   return (
     <>
       <Disclosure
@@ -87,6 +87,7 @@ export default function Navbar() {
                       {({ active }) => (
                         <span
                           role="button"
+                          onClick={() => logOut()}
                           className={classNames(
                             active ? "bg-gray-100" : "",
                             "block px-4 py-2 text-sm text-gray-700 cursor-pointer"
